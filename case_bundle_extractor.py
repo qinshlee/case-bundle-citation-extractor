fe4f09515b84eabc676dcb7f957e42fe20e86484
qinshlee <131509177+qinshlee@users.noreply.github.com>
2024-01-18 13:39:19 +0000

    Minor updates to error messages

--- case_bundle_extractor.py
+++ case_bundle_extractor.py
@@ -33,30 +33,29 @@ user_input1 = st.selectbox('Step 2: Please select from this list.', options, ind
 if user_input1 == "Acts, Codes & Charters":
 	text_A = "(?:s|ss|section)"
 	text_B = "(?:Act|Code|Charter)"
-elif user_input1 == "Laws":
-        text_A = "(?:art|article)"
-        text_B = "Law"
-elif user_input1 == "Rules of Court":
-        text_A = "(?:o|ordinance)"
-        text_B = "(?:ROC|Rules\sof\sCourt)"
-elif user_input1 == "Constitution & the Independence Agreement":
-        text_A = "(?:art|article)"
-        text_B = "(?:Constitution|Independence)"
-else: 
-        st.markdown('No results.')
+if user_input1 == "Laws":
+    text_A = "(?:art|article)"
+    text_B = "Law"
+if user_input1 == "Rules of Court":
+    text_A = "(?:o|ordinance)"
+    text_B = "(?:ROC|Rules\sof\sCourt)"
+if user_input1 == "Constitution & the Independence Agreement":
+    text_A = "(?:art|article)"
+    text_B = "(?:Constitution|Independence)"
     
-if user_input1 != None:
-	if uploaded_file == None:
-		with suppress(NameError):
-			st.markdown('Please upload file in Step 1 first.')
-	else:
-		regex = fr'(?<=\s){text_A}\s.+?of\sthe\s.{{0,85}}{text_B}\s.{{0,125}}(?=\s)'
-		regex_object = re.compile(regex, re.IGNORECASE)
-		results1 = re.findall(regex_object, court_sub)
-		st.write('Here are the results:\n')
-		container1 = st.container(border=True)
-		for i in results1:
-			container1.write(i)
+if uploaded_file == None:
+	with suppress(NameError):
+		st.markdown('Please upload file in Step 1 first.')
+elif user_input1 == None and uploaded_file != None:
+	st.markdown('No results.')
+else:
+	regex = fr'(?<=\s){text_A}\s.+?of\sthe\s.{{0,85}}{text_B}\s.{{0,125}}(?=\s)'
+	regex_object = re.compile(regex, re.IGNORECASE)
+	results1 = re.findall(regex_object, court_sub)
+	st.write('Here are the results:\n')
+	container1 = st.container(border=True)
+	for i in results1:
+		container1.write(i)
 
 # Streamlit dropdown options for free-text searches for abbreviations with citation
 
@@ -68,7 +67,7 @@ if uploaded_file == None:
 elif user_input2 == None and uploaded_file != None:
 	st.markdown('No results.')
 elif re.fullmatch(r'[A-Z]{2,10}', user_input2) == None and uploaded_file != None:
-        st.markdown('Invalid keyword.')
+    st.markdown('Invalid keyword.')
 else:	
 	regex2 = fr'(?<=\s)(?:[sS]{{1,2}}|[sS]ection|[aA]rt|[aA]rticle|[oO]|[oO]rdinance)\s.+?of\sthe\s.{{0,85}}?{user_input2}\s.{{0,125}}(?=\s)'
 	regex_object2 = re.compile(regex2)
